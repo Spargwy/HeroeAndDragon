02ad66a25b0cbf53d27db13cd7ee307314373d66
Slith41 <nameisconfidentialinformation@gmail.com>
2021-01-20 05:17:19 +0600

    dich added!

--- heroHeal.go
+++ heroHeal.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func heroHeal(hero Hero, dragon Dragon, maxHealth int) int {
 	health := hero.health
+	fmt.Println("HEEEEEEEEEEEEEEEEEEEEEEEEEEEY", health)
 	fmt.Println(health)
 	if health <= maxHealth-20 {
 		health = health + 20
--- main.go
+++ main.go
@@ -64,59 +64,61 @@ func main() {
 	maxHealth := hero.maxHealth
 	move := 1
 	for hero.health > 0 && dragon.health > 0 {
-		fmt.Printf("Choose the next action: ")
-		fmt.Scanf("%s", &action)
-		switch action {
-		case "attack":
-			fmt.Printf("If you want to use weapon, enter it's name: ")
-			var weaponName string
-			fmt.Scanf("%s", &weaponName)
-			if weaponName == "pan" {
-				weapon := usingWeapon(pan.usedTimes, pan.name, pan.damage)
-				dragon.health, pan.damage = heroAttack(hero, dragon, weapon)
-				pan.usedTimes++
-			} else if weaponName == "crossbow" {
-				weapon := usingWeapon(crossbow.usedTimes, crossbow.name, crossbow.damage)
-				dragon.health, crossbow.damage = heroAttack(hero, dragon, weapon)
+		dragon.health, hero.health = naturalDisasters(dragon, hero)
+		if dragon.health > 0 && hero.health > 0 {
+			fmt.Printf("Choose the next action: ")
+			fmt.Scanf("%s", &action)
+			switch action {
+			case "attack":
+				fmt.Printf("If you want to use weapon, enter it's name: ")
+				var weaponName string
+				fmt.Scanf("%s", &weaponName)
+				if weaponName == "pan" {
+					weapon := usingWeapon(pan.usedTimes, pan.name, pan.damage)
+					dragon.health, pan.damage = heroAttack(hero, dragon, weapon)
+					pan.usedTimes++
+				} else if weaponName == "crossbow" {
+					weapon := usingWeapon(crossbow.usedTimes, crossbow.name, crossbow.damage)
+					dragon.health, crossbow.damage = heroAttack(hero, dragon, weapon)
 
-				crossbow.usedTimes++
-				fmt.Println(crossbow.usedTimes)
-			} else {
-				weapon := usingWeapon(standartSword.usedTimes, standartSword.name, standartSword.damage) //можно не вызывать. Хочу, чтобы default выполнился. Я сним час мучился. Надо же себя утещить
-				dragon.health, standartSword.damage = heroAttack(hero, dragon, weapon)
-			}
+					crossbow.usedTimes++
+					fmt.Println(crossbow.usedTimes)
+				} else {
+					weapon := usingWeapon(standartSword.usedTimes, standartSword.name, standartSword.damage) //можно не вызывать. Хочу, чтобы default выполнился. Я сним час мучился. Надо же себя утещить
+					dragon.health, standartSword.damage = heroAttack(hero, dragon, weapon)
+				}
 
-		case "heal":
-			hero.health = heroHeal(hero, dragon, maxHealth)
-
-		default:
-			fmt.Printf("If you will not choose the correct actions, heal will be execute automatically")
-			if hero.armor <= 0 {
+			case "heal":
 				hero.health = heroHeal(hero, dragon, maxHealth)
-			} else {
+
+			default:
+				fmt.Printf("If you will not choose the correct actions, heal will be execute automatically")
+				if hero.armor <= 0 {
+					hero.health = heroHeal(hero, dragon, maxHealth)
+				} else {
+
+				}
 
 			}
+			if dragon.health > 0 {
+				hero.health, hero.armor = dragonAttack(hero, dragon)
+			}
+
+			fmt.Printf("\n\n\n#++++++++++++++++++++++++++++++++++++\n")
+			fmt.Printf("#move: %d\n", move)
+			fmt.Printf("#Your Hero's health = %d. \n", hero.health)
+			fmt.Printf("#Dragon's health = %d\n", dragon.health)
+			fmt.Printf("Your Hero's armor = %d. \n", hero.armor)
+			fmt.Printf("#++++++++++++++++++++++++++++++++++++\n\n\n")
+
+			if hero.health <= 0 {
+				fmt.Printf("Dragon win! The battle lasted %d moves!\n", move)
+			} else if dragon.health <= 0 {
+				fmt.Printf("Hero win! The battle lasted %d moves!\n", move)
+			}
+
+			move++
 
 		}
-		if dragon.health > 0 {
-			hero.health, hero.armor = dragonAttack(hero, dragon)
-		}
-
-		fmt.Printf("\n\n\n#++++++++++++++++++++++++++++++++++++\n")
-		fmt.Printf("#move: %d\n", move)
-		fmt.Printf("#Your Hero's health = %d. \n", hero.health)
-		fmt.Printf("#Dragon's health = %d\n", dragon.health)
-		fmt.Printf("Your Hero's armor = %d. \n", hero.armor)
-		fmt.Printf("#++++++++++++++++++++++++++++++++++++\n\n\n")
-
-		if hero.health <= 0 {
-			fmt.Printf("Dragon win! The battle lasted %d moves!\n", move)
-		} else if dragon.health <= 0 {
-			fmt.Printf("Hero win! The battle lasted %d moves!\n", move)
-		}
-
-		move++
-
 	}
-
 }
