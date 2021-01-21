4a518e184b3a2a95e4762a6850493ac78c7f6037
Slith41 <nameisconfidentialinformation@gmail.com>
2021-01-21 22:17:52 +0600

    added tests for 3 functions. One work not correctly

--- hero_heal.go
+++ hero_heal.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func heroHeal(hero Hero, dragon Dragon, maxHealth int) int {
+func heroHeal(hero Hero, maxHealth int) int {
 	health := hero.health
 	health = health + 20
 	fmt.Printf("You healed your hero to 20 HP\n")
