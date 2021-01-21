3067b5ccd264a3f52a9119d8a27756eb9ec47829
Slith41 <nameisconfidentialinformation@gmail.com>
2021-01-21 14:52:02 +0600

    hero_tiredness to separate file

--- hero_Attack.go
+++ hero_Attack.go
@@ -6,21 +6,6 @@ import (
 	"time"
 )
 
-func heroTiredness(hero Hero) int {
-	var damageReduction int
-	if hero.health <= (hero.maxHealth/100*30) && hero.health > hero.maxHealth/100*20 {
-		damageReduction = 5
-	} else if hero.health <= hero.maxHealth/100*20 && hero.health > hero.maxHealth/100*10 {
-		damageReduction = 10
-	} else if hero.health <= hero.maxHealth/100*10 {
-		damageReduction = 20
-	} else {
-		damageReduction = 0
-	}
-
-	return damageReduction
-}
-
 //строится на основе weapon
 func heroAttack(hero Hero, dragon Dragon, weapon Weapon) int {
 	damageReduction := heroTiredness(hero)
