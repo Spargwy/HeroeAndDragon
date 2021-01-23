45a5a77ebef7775a9d384f6d2dbdc0e958ca3a9f
Slith41 <nameisconfidentialinformation@gmail.com>
2021-01-23 08:41:00 +0600

    edit variables names, fix random for hero and dragon attacks

--- hero_chance_to_attack_test.go
+++ hero_chance_to_attack_test.go
@@ -7,16 +7,16 @@ import (
 
 func TestHeroChanceToAttack(t *testing.T) {
 	weaponMissChance := 0
-	chanceIsGood := heroChanceToAttack(weaponMissChance, 18)
-	wantChanceIsGood := true
-	if chanceIsGood != wantChanceIsGood {
-		t.Fatalf("ERROR, chance is good must be equal %v, but equal %v", wantChanceIsGood, chanceIsGood)
+	successfulAttack := heroChanceToAttack(weaponMissChance, 18)
+	wantsuccessfullyAttack := true
+	if successfulAttack != wantsuccessfullyAttack {
+		t.Fatalf("ERROR, chance is good must be equal %v, but equal %v", wantsuccessfullyAttack, successfulAttack)
 	}
 	weaponMissChance = 0
-	chanceIsGood = heroChanceToAttack(weaponMissChance, 19)
-	wantChanceIsGood = true
-	if chanceIsGood != wantChanceIsGood {
-		t.Fatalf("ERROR, chance is good must be equal %v, but equal %v", wantChanceIsGood, chanceIsGood)
+	successfulAttack = heroChanceToAttack(weaponMissChance, 19)
+	wantsuccessfullyAttack = true
+	if successfulAttack != wantsuccessfullyAttack {
+		t.Fatalf("ERROR, chance is good must be equal %v, but equal %v", wantsuccessfullyAttack, successfulAttack)
 	}
 	fmt.Print("OK\n")
 
